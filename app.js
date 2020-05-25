b7e6adadec96d4b052149bb17852fa16215039e1
mhtaldmr <mht.alidemir@gmail.com>
2020-05-26 01:46:08 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+
+//require the needed packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const path = require('path');
+const _ = require('lodash');
+
+// to use express package
+const app = express();
+// to view the ejs engine
+app.set('view engine', 'ejs');
+//to use bodyparse or get the inputs from user
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+//to use the files in public; in this case css file
+app.use(express.static("public"));
+
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
+const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+let posts = [];
+
+//rendering home page
+app.get("/", function (req, res) {
+    res.render("home", {
+        homeContentp1: homeStartingContent,
+        postItems: posts
+    });
+});
+
+//rendering about page
+app.get("/about", function (req, res) {
+    res.render("about", {
+        aboutContentp1: aboutContent
+    });
+});
+
+//rendering compact page
+app.get("/contact", function (req, res) {
+    res.render("contact", {
+        contactContentp1: contactContent
+    });
+});
+
+//rendering compose page
+app.get("/compose", function (req, res) {
+    res.render("compose");
+});
+
+
+//page to see posts individually
+app.get("/posts/:id", function (req, res) {
+
+    const postTitleParam = _.lowerCase(req.params.id);
+
+    posts.forEach(post => {
+        const storedTitleParam = _.lowerCase(post.title);
+        
+        if (storedTitleParam === postTitleParam) {
+        
+            res.render("post", {
+                postTitleInPosts : postTitleParam,
+                postContentInPosts : post.content
+            });
+        }
+    });
+});
+
+
+//compose page inputs taking and redirecting to home
+app.post("/compose", function (req, res) {
+
+    post = {
+        title: req.body.composeTitle,
+        content: req.body.composePost
+    };
+
+    posts.push(post);
+    res.redirect("/");
+});
+
+
+
+//port to listen in browser
+app.listen(3000, function () {
+    console.log("Server is running now..");
+});
